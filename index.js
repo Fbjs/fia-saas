51ce79dae7a2f501ed7e5418e20d93dced6c7663
Tu Nombre <fibaceta.arce@gmail.com>
2025-04-28 21:41:15 -0400

    feat: agregar conexión a MongoDB en FIA

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const app = require('./src/app');
 const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
+
 dotenv.config();
+
+// Conectar a base de datos
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
